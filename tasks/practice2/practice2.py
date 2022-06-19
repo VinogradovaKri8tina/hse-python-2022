77a4e5110f41300fc82e597cfab7ff3459b5ea03
VinogradovaKri8tina <107803504+VinogradovaKri8tina@users.noreply.github.com>
2022-06-20 01:56:05 +0300

    Update practice2.py

--- tasks/practice2/practice2.py
+++ tasks/practice2/practice2.py
@@ -1,5 +1,5 @@
-import random
 from typing import Iterable
+import random
 
 UNCULTURED_WORDS = ('kotleta', 'pirog')
 
@@ -8,12 +8,12 @@ def greet_user(name: str) -> str:
     """
     Генерирует приветственную фразу.
     Приветствие не может состоять только из одного имени пользователя.
-
     :param name: имя пользователя
     :return: приветствие
     """
 
     # пиши код здесь
+
     greeting = "Hello " + name
     return greeting
 
@@ -21,18 +21,16 @@ def greet_user(name: str) -> str:
 def get_amount() -> float:
     """
     Генерируем случайную сумму на счете.
-
     Сумма:
     - в диапазоне от 100 до 1000000
     - float
     - не больше 2-х знаков после запятой
-
     :return: случайную сумму на счете
     """
 
     # пиши код здесь
-    
-    amount  = float(random.randrange(100, 1000000))
+
+    amount = round(random.uniform(100, 100000), 2)
     return amount
 
 
@@ -40,25 +38,23 @@ def is_phone_correct(phone_number: str) -> bool:
     """
     Функция проверяет, что номер телефона соответствует следующему формату:
     +7xxxxxxxxxx, где x - цифра от 0 до 9
-
     :param phone_number: предполагаемый номер телефона
     :return: буленовское значение - bool: True - если номер корректны,
                                           False - если номер некорректный
     """
 
     # пиши код здесь
-    if (phone_number[0] == '+' and phone_number[1] == '7' and phone_number[2:].isdigit() == True and
-            len(phone_number) == 12):
-        result = True
-    else:
-        result = False
+
+    result = len(phone_number) == 12 and phone_number[0:2] == "+7" and phone_number[2:].isdigit()
+
+    return result
+
 
 def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
     """
     Проверяет возможность осуществления перевода.
     Перевод возможен если выполняется условие:
     - текущий счет (current_amount) больше или равен сумме перевода (transfer_amount)
-
     :param current_amount: текущий счет
     :param transfer_amount: сумма перевода
     :return: буленовское значение - bool: True - если перевод возможен,
@@ -66,29 +62,27 @@ def is_amount_correct(current_amount: float, transfer_amount: str) -> bool:
     """
 
     # пиши код здесь
-    if(current_amount >= (float(transfer_amount))):
-        result = True
-    else:
-        result = False
+
+    result = current_amount >= float(transfer_amount)
+
     return result
 
 
 def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
     """
     Модерирует текст по следующим правилам.
-
     Требования к тексту:
     - Первая буква заглавная, остальные буквы только в нижнем регистре
     - отсутствую лишние пробелы
     - фильтруются 'опасные' символы: " ' (двойные и одинарные кавычки)
     - слова, перечисленные в переменной uncultured_words заменяются на аналогичное количество знаков #
-
     :param text: исходный текст
     :param uncultured_words: список запрещенных слов
     :return: текст, соответсвующий правилам
     """
 
     # пиши код здесь
+
     text = text.strip()
     text = text.capitalize()
     text = text.replace("'", '')
@@ -98,6 +92,8 @@ def moderate_text(text: str, uncultured_words: Iterable[str]) -> str:
             zamen = "#" * len(sl)
             text = text.replace(sl, zamen)
     result = text
+    
+    return result
 
 
 def create_request_for_loan(user_info: str) -> str:
@@ -120,7 +116,9 @@ def create_request_for_loan(user_info: str) -> str:
     """
 
     # пиши код здесь
-    array_data = user_info.split(",")
-    result = f'Фамилия: {array_data[0]}\nИмя: {array_data[1]}\nОтчество: {array_data[2]}\nДата рождения: {array_data[3]}\nЗапрошенная сумма: {array_data[4]}'
+
+    baza = user_info.split(',')
+    result = 'Фамилия: {}\nИмя: {}\nОтчество: {}\nДата рождения: {}\nЗапрошенная сумма: {}'.format(*baza)
+
 
     return result

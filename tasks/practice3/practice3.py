4a312d036e8e5c070b20596e8b90b2a21c1b8adf
VinogradovaKri8tina <107803504+VinogradovaKri8tina@users.noreply.github.com>
2022-06-20 01:44:05 +0300

    Update practice3.py

--- tasks/practice3/practice3.py
+++ tasks/practice3/practice3.py
@@ -1,25 +1,20 @@
 from pathlib import Path
 from typing import Dict, Any, List, Optional
-import csv
+
 
 def count_words(text: str) -> Dict[str, int]:
     """
     Функция для подсчета слов в тексте.
-
     При подсчете слов - все знаки препинания игнорируются.
     Словом считается непрерывная последовательность длиной больше одного
     символа, состоящая из букв в диапазоне A-Z и a-z.
     Если в последовательности присутствует цифра - это не слово.
-
     Hello - слово
     Hello7 - не слово
-
     При подсчете слов регистр букв не имеет значения.
-
     Результат выполнения функции словарь, в котором:
     ключ - слово в нижнем регистре
     значение - количество вхождений слов в текст
-
     :param text: текст, для подсчета символов
     :return: словарь, в котором:
              ключ - слово в нижнем регистре
@@ -27,29 +22,20 @@ def count_words(text: str) -> Dict[str, int]:
     """
 
     # пиши свой код здесь
-    d = {}
-    text = re.sub(r'[^\w\s]', '', text)
-    text = text.lower().split()
-    for word in text:
-        if (word.isalpha() and len(word) > 1):
-            if (d.get(word) == None):
-                d[word] = 1
-            else:
-                d[word] += 1
+
     return {}
-    return d
+
 
 def exp_list(numbers: List[int], exp: int) -> List[int]:
     """
     Функция, которая возводит каждый элемент списка в заданную степень
-
     :param numbers: список, состоящий из натуральных чисел
     :param exp: в какую степень возвести числа в списке
     :return: список натуральных чисел
     """
 
     # пиши свой код здесь
-    return [x ** exp for x in numbers]
+
     return []
 
 
@@ -58,7 +44,6 @@ def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) 
     Функция для расчета кешбека по операциям.
     За покупки в обычных категориях возвращается 1% от стоимости покупки
     За покупки в special_category начисляют 5% от стоимости покупки
-
     :param operations: список словарей, содержащих поля
            amount - сумма операции
            category - категория покупки
@@ -78,10 +63,8 @@ def get_cashback(operations: List[Dict[str, Any]], special_category: List[str]) 
 def get_path_to_file() -> Optional[Path]:
     """
     Находит корректный путь до тестового файла.
-
     Если запускать тесты из pycharm - начальная папка - tests
     Если запускать файлы через make tests - начальная папка - корень проекта
-
     :return: путь до тестового файла tasks.csv
     """
     if Path().resolve().name == 'tests':
@@ -97,28 +80,18 @@ def csv_reader(header: str) -> int:
     уникальных элементов в столбце.
     Столбец выбирается на основе имени заголовка,
     переданного в переменной header.
-
     Обратите внимание на структуру файла!
     Первая строка - строка с заголовками.
     Остальные строки - строки с данными.
-
     Файл для анализа: tasks.csv
     Для того чтобы файл корректно открывался в тестах:
     для получения пути до файла - используйте функцию get_path_to_file
     которая определена перед функцией.
-
     CSV анализируем с помощью встроенной библиотеки csv
-
     :param header: название заголовка
     :return: количество уникальных элементов в столбце
     """
 
     # пиши свой код здесь
-    data = []
-    with open(get_path_to_file()) as csv_file:
-        csv_reader = csv.DictReader(csv_file, delimiter="," )
-        for row in csv_reader:
-            data.append(row[header])
-        amount = len(set(data))
-    return amount
+
     return 0
